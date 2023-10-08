adb9a5a1d245288c690a8caff218cb9bea20d45f
Matheus Vinicius Bianchini <mvbbianchini@hotmail.com>
2023-10-08 12:15:39 -0300

    chore: added kotlin-parcelize plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
