2b70dc6391de19de3d07444fbf16d6f911332db8
Matheus Vinicius Bianchini <mvbbianchini@hotmail.com>
2023-10-08 12:16:05 -0300

    feat: added buttons components

--- app/src/main/java/com/bianchini/vinicius/matheus/cupcake/ui/components/ButtonsComponents.kt
+++ app/src/main/java/com/bianchini/vinicius/matheus/cupcake/ui/components/ButtonsComponents.kt
@@ -5,11 +5,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -54,5 +59,31 @@ fun ButtonPrimary(
                 fontWeight = FontWeight.Bold
             )
         }
+    }
+}
+
+@Composable
+fun ButtonClicked(
+    value: String,
+    onClick: () -> Unit,
+    clickedColor: Color,
+    defaultColor: Color
+) {
+    var isClicked by remember { mutableStateOf(false) }
+    Button(
+        onClick = {
+            isClicked = !isClicked
+            onClick()
+        },
+        modifier = Modifier.wrapContentSize(),
+        colors = if (isClicked) {
+            ButtonDefaults.buttonColors(clickedColor)
+        } else ButtonDefaults.buttonColors(defaultColor),
+    ) {
+        Text(
+            text = value,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
