d58ad3b111b2af8c23422af32c7f3de90f4917b8
Matheus Vinicius Bianchini <mvbbianchini@hotmail.com>
2023-10-08 12:23:19 -0300

    feat: added product size class

--- app/src/main/java/com/bianchini/vinicius/matheus/cupcake/feature/home/domain/Food.kt
+++ app/src/main/java/com/bianchini/vinicius/matheus/cupcake/feature/home/domain/Food.kt
@@ -4,6 +4,17 @@ data class Food(
     val id: Int,
     val name: String,
     val description: String,
+    val size: List<Size>,
     val prince: Double,
     val image: String
-)
+) {
+
+    enum class Size(
+        val value: Int,
+        val label: String
+    ) {
+        SMALL(0, "Pequeno"),
+        MEDIUM(1, "Médio"),
+        LARGE(2, "Grande")
+    }
+}
